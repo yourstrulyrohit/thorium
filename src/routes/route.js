10fd33bdfce006c3da871bf80d1f8f39f874670b
ROHIT <yourstrulyrohit@gmail.com>
2022-02-25 19:42:49 +0530

    post-query assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,109 +1,51 @@
 const express = require('express');
 const router = express.Router();
-
-
-
-let players = [
-
+let persons= [
     {
-        "name": "manish",
-        "dob": "1/1/1995",
-        "gender": "male",
-        "city": "jalandhar",
-        "sports": ["swimming"],
-
-        "bookings": [
-
-            {
-                "bookingNumber": 1,
-                "sportId": "",
-                "centerId": "",
-                "type": "private",
-                "slot": '16286598000000',
-                "bookedOn": '31/08/2021',
-                "bookedFor": '01/09/2021'
-            },
-            {
-                "bookingNumber": 2,
-                "sportId": "",
-                "centerId": "",
-                "type": "private",
-                "slot": '16286518000000',
-                "bookedOn": '31/08/2001',
-                "bookedFor": '01/09/2001'
-            }]
-    },
-
-    {
-        "name": "gopal",
-        "dob": "1/09/1995",
-        "gender": "male",
-        "city": "delhi",
-        "sports": [
-            "soccer"
-        ],
-        "bookings": []
-    },
-
-    {
-        "name": "lokesh",
-        "dob": "1/1/1990",
-        "gender": "male",
-        "city": "mumbai",
-        "sports": ["soccer"],
-        "bookings": []
-    },
-]
-
-router.post("/players", (req, res) => {
-
-    let newPlayer = req.body
-    let cnt = 0;
-
-    for (let i = 0 ; i < players.length ; i++) {
-
-        if (players[i]['name'] == newPlayer['name']) {
-            cnt++
-            res.send({ "data": "players", "status": "player already exists" })
-        }
-
-    }
-    if (cnt == 0) {
-        players.push(newPlayer)
-        res.send(players)
-    }
-});
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+   router.post("/postquery", function (req, res) {
+    
+      let votingAge = req.query.age;
+      let validVoters = [];
+  
+       for (let i = 0; i < persons.length; i++) {
+  
+         if (persons[i]["age"] > votingAge) {
+            persons[i]["votingStatus"] = true;
+             validVoters.push(persons[i]);
+           }
+  
+       }
+  
+       res.send(validVoters);;
+    
 
 
-router.post("/players/:playerName/bookings/:bookingId", (req, res) => {
 
-    let playerName = req.params.playerName
-    let bookings = req.body
-    let bookingId = req.params.bookingId
-
-    let cnt = 0;
-    for (let i = 0; i < players.length; i++) {
-
-        if (players[i]['name'] == playerName) {
-            cnt++
-
-            if (players[i]['bookings'][i]) {
-                res.send("booking was already processed")
-
-            } else {
-                players[i].bookings.push(bookings)
-                res.send(players[i])
-                break;
-            }
-        }
-    }
-
-
-    if (cnt == 0) {
-        res.send({ "data": "players", "status": "something relevant about player not being found" })
-    }
-
-
-});
-
-module.exports = router;
+ })
+ 
+ module.exports = router;
