c7f3f6627bb556da2a2a1c6f8cd4392fa62cd273
ROHIT <yourstrulyrohit@gmail.com>
2022-03-11 03:41:54 +0530

    try/catch & statuscodes added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,13 +2,10 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
+  
   let data = abcd.body;
   let savedData = await userModel.create(data);
-  // console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  xyz.status(200).send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -17,7 +14,7 @@ const loginUser = async function (req, res) {
 
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
-    return res.send({
+    return res.status(401).send({
       status: false,
       msg: "username or the password is not corerct",
     });
@@ -31,27 +28,30 @@ const loginUser = async function (req, res) {
     "functionup-thorium"
   );
   res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
+  res.status(200).send({ status: true, data: token });
 };
 
 const getUserData = async function (req, res) {
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
 
-  if (!token) return res.send({ status: false, msg: "token must be present" });
+  if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+
 
   console.log(token);
   
   let decodedToken = jwt.verify(token, "functionup-thorium");
   if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+    return res.status(403).send({ status: false, msg: "token is invalid" });
 
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
-
-  res.send({ status: true, data: userDetails });
+    return res.status(401).send({ status: false, msg: "No such user exists" });
+    if (token.userId!= userId){
+      return res.status(403).send({status:false,msg:"invalid userid"})
+  }
+  res.status(200).send({ status: true, data: userDetails });
 };
 
 const updateUser = async function (req, res) {
@@ -59,35 +59,35 @@ const updateUser = async function (req, res) {
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   if (!user) {
-    return res.send("No such user exists");
+    return res.status(401).send("No such user exists");
   }
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  res.status(200).send({ status: updatedUser, data: updatedUser });
 };
 
 const postMessage = async function (req, res) {
     let message = req.body.message
     let token = req.headers["x-auth-token"]
-    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    if(!token) return res.status(401).send({status: false, msg: "token must be present in the request header"})
     let decodedToken = jwt.verify(token, 'functionup-thorium')
 
-    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+    if(!decodedToken) return res.status(403).send({status: false, msg:"token is not valid"})
     
     let userToBeModified = req.params.userId
     let userLoggedIn = decodedToken.userId
 
-    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+    if(userToBeModified != userLoggedIn) return res.status(403).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
 
     let user = await userModel.findById(req.params.userId)
-    if(!user) return res.send({status: false, msg: 'No such user exists'})
+    if(!user) return res.status(401).send({status: false, msg: 'No such user exists'})
     
     let updatedPosts = user.posts
     updatedPosts.push(message)
     let updatedUser = await userModel.findOneAndUpdate({_id: user._id},{posts: updatedPosts}, {new: true})
 
-    return res.send({status: true, data: updatedUser})
+    return res.status(200).send({status: true, data: updatedUser})
 
     
 };
@@ -95,7 +95,7 @@ const deleteUser = async function (req, res) {
   let userId = req.params.userId;
   let ChangeUserProperty = await userModel.findOneAndUpdate( { _id : userId },{$set : {isDeleted : true }})
   let deleteUser = await userModel.findByIdAndDelete(userId)
-  res.send({msg :"user deleted Succesfully" ,deleteUser })
+  res.status(200).send({msg :"user deleted Succesfully" ,deleteUser })
 
 };
 const createPost = async function (req, res) {
@@ -106,7 +106,7 @@ const createPost = async function (req, res) {
   let updatedPosts = user.posts
       updatedPosts.push(message)
   let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: updatedPosts }, { new: true })
-  return res.send({ status: true, data: updatedUser })
+  return res.status(200).send({ status: true, data: updatedUser })
 
 }
 module.exports.createPost=createPost
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,21 +3,23 @@ const jwt = require('jsonwebtoken')
 
 let authenticate = function (req, res, next) {
 
-try {
-
+try{
     let token = req.headers["x-auth-token"];
     if (!token) {
-        return res.send({ status: false, msg: "token must be present" });
+        return res.status(401).send({ status: false, msg: "token must be present" });
     }
-
+    
     let decodedToken = jwt.verify(token, "functionup-thorium");
 
     if (decodedToken.userId !== req.params.userId) {
-        return res.send({ msg: "you must have to login first" })
+        return res.status(403).send({ msg: "you must have to login first" })
     }
     next()
+}
+catch(err){ res.status(500).send({ msg: "error"})
 
-} catch (error) { res.send(error) }
+}
+
 }
 
 
